753c138bc32f82bf3ac5653faaf7f8a5f021a1f1
Ndoria <k.ndoriamurigu@gmail.com>
2024-07-13 12:09:19 +0300

    complete labs

---
+++ index.js
@@ -0,0 +1,15 @@
+let num1 = 2000
+let num2 = 100
+
+function divide() {
+    return num1 / num2;
+}
+
+const square = (input) => {
+    // console.log(input)
+    return input*input;
+}
+
+const add = (a, b) => {
+    return a + b;
+}
